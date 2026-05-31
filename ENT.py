4231343c2bdea131222adb347206fe47288561d7
Khalid038 <Drewyeezy2@gmail.com>
2026-05-31 15:52:26 +0100

    Add files via upload

---
+++ ENT.py
@@ -0,0 +1,33 @@
+# File: initials.py
+# Description: print out my initials
+# Assignment number: 2
+#
+# Name: Emmanuel Nana Tuah
+# Student ID: 2425400380
+# Email: Tuahe6025@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Emmanuel Nana Tuah, this programming assignment is my own work
+# and I have  not provided this code to any other student.
+
+
+def main():
+    # print Initials ENT In stylized large block letters using the letters themselves
+
+    print()
+    print("...ENT")
+    print()
+    print("...EEEEEEEEEEEE........NN......NN...TTTTTTTTTTTT")
+    print("...EE..................NNN.....NN...TTTTTTTTTTTT")
+    print("...EE..................NNNN....NN........TT.....")
+    print("...EE..................NN.NN...NN........TT.....")
+    print("...EEEEEEEEEE..........NN..NN..NN........TT.....")
+    print("...EEEEEEEEEE..........NN...NN.NN........TT.....")
+    print("...EE..................NN....NNNN........TT.....")
+    print("...EE..................NN.....NNN........TT.....")
+    print("...EE.............**...NN......NN...**...TT...**")
+    print("...EEEEEEEEEEEE...**...NN......NN...**...TT...**")
+    print()
+
+
+main()
